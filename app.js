6cfc3769f6de94f867172e02871ecaeabc043250
Alok Patel <alokpatel1313@gmail.com>
2024-07-18 23:11:39 +0530

    add new

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// const div = React.createElement("Div",{id : parent}, React.createElement("Div",{id : "child"},React.createElement("h1",{},"this is h1 tag")));
-
-
+// const div = React.createElement("Div",{id : parent}, 
+// React.createElement("Div",{id : "child"},
+//     React.createElement("h1",{},"this is h1 tag")));
 // const heading = React.createElement("h1",
 //     {id:"head"},
 //     "helo world print in react");
