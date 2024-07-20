bc85da8bbd94ba3ea56e90fd522885e5eaf5a98a
Alok Patel <alokpatel1313@gmail.com>
2024-07-20 09:49:12 +0530

    https://github.com/Alok7326/Namaste-React.git

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-// const div = React.createElement("Div",{id : parent}, 
-// React.createElement("Div",{id : "child"},
-//     React.createElement("h1",{},"this is h1 tag")));
-// const heading = React.createElement("h1",
-//     {id:"head"},
-//     "helo world print in react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div",{ id : "parent"} ,[
+React.createElement("div",{id: "child"},[
+React.createElement("h1",{} ,"i m h1 tag "),
+React.createElement("h2",{} ,"i m h2 tag "),
+]),
+
+React.createElement("div",{id: "child2"},[
+    React.createElement("h1",{} ,"i m h1 tag "),
+    React.createElement("h2",{} ,"i m h2 tag "),
+]),
+]);
+
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(div);
+root.render(parent);
